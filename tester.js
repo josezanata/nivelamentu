49cd03fdad99b5615f8b18047398638c94059cb8
josezanata <jose.krais@escola.pr.gov.br>
2026-04-08 11:54:04 -0300

    Add files via upload

---
+++ tester.js
@@ -0,0 +1,30 @@
+//1. Exibir a mensagem olá, mundo.
+console.log("Olá, Mundo!");
+
+//2. Crie uma variavel com seu nome e exiba o valor
+let nome = "zanatta";
+console.log(nome);
+
+//3. some 2 numeros e exiba o resultado
+let soma = 10 + 4;
+console.log(soma);
+
+//4. Subtraia 2 numeros e exiba o resultado
+let subtracao = 10 - 4;
+console.log(subtracao);
+
+//5. Descubra o resto da divisao e exiba
+let resto = 10 % 4;
+console.log(resto);
+
+//6. crie uma variavel booleana e exiba o seu valor
+let estaCalor = Boolean(true);
+console.log(estaCalor);
+
+//7. verifique se um numero é maior que o outro e exiba o reultado.
+let maiorque = 1 > 5;
+console.log(maiorque);
+
+//8. Multiplique dois numeros e exiba o resultado
+let multiplicacao = 10 * 4;
+console.log(multiplicacao);
