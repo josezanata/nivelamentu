519229e0a0c8aaccc55bd6adc8e28ad83d0452a1
josezanata <jose.krais@escola.pr.gov.br>
2026-04-09 09:08:23 -0300

    Add files via upload

--- tester.js
+++ tester.js
@@ -25,6 +25,52 @@ console.log(estaCalor);
 let maiorque = 1 > 5;
 console.log(maiorque);
 
+//8. Crie duas variaveis com algum valor e exiba a concatenação
+let num1 = "teste";
+let num2 = "teste";
+console.log(`teste ${num1} ${num2}`);
+
+//9. Descubra o tipo de uma variavel
+let valor = 100;
+console.log(typeof valor);
+
+//10. converta numero para string
+let num = 50;
+console.log(String(num));
+
+//11. string para numero 
+let texto = "123";
+console.log(number(texto));
+
+//12. exiba no console numero de 0 10
+for(let i =0; i<=10; i++){
+    console.log(i);
+}
+
+//13. exiba apenas numero partes até 20.
+for(let i =0; i<=20; i+=2){
+    console.log(i);
+}
+
+//14. Mostre no console numero de 10 até 1.
+for(let i =10; i>0; i--){
+    console.log(i);
+}
+
+//15. some numeros de 1 a 5
+for(let i =1; i<=5; i++){
+    somaLoop+=i;
+    console.log
+}
+console.log(somaLoop);
+
+//16. faça a tabuada do 3
+for(let i =1; i<=10; i++){
+    console.log("3 x "+i+"="+3*i);
+}
+
+
+
 //8. Multiplique dois numeros e exiba o resultado
 let multiplicacao = 10 * 4;
 console.log(multiplicacao);
